ca83f545bf34ee620760013b7f1b6597ac406289
Jonas Happ <github@jonashapp.com>
2024-01-28 18:45:03 +0100

    Success Calculate mg/ml per 100 ml

--- 04_CoffeeProject/#Coffee Data Project.py
+++ 04_CoffeeProject/#Coffee Data Project.py
@@ -3,11 +3,60 @@
 import pandas as pd
 from icecream import ic
 
-xlsx = pd.ExcelFile("/Users/jonashapp/Documents/GitHub/MS_TF101_-001_Coffee-Project/04_CoffeeProject/Coffee Caffeine Content.xlsx")
-df = pd.read_excel(xlsx)
+data = pd.ExcelFile("/Users/jonashapp/Documents/GitHub/MS_TF101_-001_Coffee-Project/04_CoffeeProject/Coffee Caffeine Content.xlsx")
+coffee_data = pd.read_excel(data)
 
 #As I user I want to know which chain has the most caffein (mg) per amount in ml
+
+#ic((type(coffee_data)))
+ic(coffee_data)
+
 #As I user I want to know which product has the most caffein per amount
 
-ic((type(df)))
-ic(df)
+ic(coffee_data.columns) #Gives you Header Names of Rows as a List
+ic(coffee_data.shape) #Gives ou the number of columns and rows, Like length / len()
+
+#ic(coffee_data.tail(3)) #Gives you the last {number} columns as an output  
+
+chain_list = (coffee_data['Chain'])
+#ic(chain_list)
+
+chain_list_unique = pd.unique(coffee_data['Chain']) #Filters the List of Chains to return only the names once, gives out array
+
+ic(chain_list_unique)
+
+
+drinks_list_unique = pd.unique(coffee_data['Product'])
+ic(drinks_list_unique)
+
+def calc_caffeine_per_ammount(caffeine_mg, drink_size):
+
+    #should return ammount caffeine in mg per 100 ml
+
+    caffeine_per_ammount = caffeine_mg / drink_size * 100
+    caffeine_per_ammount = round(caffeine_per_ammount, 2)
+
+    return caffeine_per_ammount
+
+def execute_calculation_in_list(coffee_data): #imports old list
+
+    column_count = len(coffee_data['Chain'])  #Gets number of items in DataFramew without header
+
+    for x in range(0, column_count): #Starts at 1, not header
+
+        caffeine_mg = coffee_data.iloc[x, 2]
+        drink_size = coffee_data.iloc[x, 3]
+        
+        ic(caffeine_mg)
+        ic(drink_size)
+
+        caffein_per_ammount = calc_caffeine_per_ammount(caffeine_mg, drink_size) #100 proxy
+        
+        ic(caffein_per_ammount)
+
+    #Should execute calculation for every drink in list and return a new list with column Caffeine_Ammount
+    
+    #return new_coffee_data         
+    #DataFrame Format
+
+execute_calculation_in_list(coffee_data)
