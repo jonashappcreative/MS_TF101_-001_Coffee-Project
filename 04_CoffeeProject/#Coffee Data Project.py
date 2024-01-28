c70b6824d3b9af96e7176de13788e53e9a87bd8f
Jonas Happ <github@jonashapp.com>
2024-01-28 15:00:17 +0100

    install test of icecream

---
+++ 04_CoffeeProject/#Coffee Data Project.py
@@ -0,0 +1,15 @@
+#Coffee Data Project
+
+import pandas as pd
+from icecream import ic
+
+
+xlsx = pd.ExcelFile("/Users/jonashapp/Desktop/Arbeit/00_Masterschool/04_CoffeeProject/Coffee Caffeine Content.xlsx")
+
+#As I user I want to know which chain has the most caffein (mg) per amount in ml
+#As I user I want to know which product has the most caffein per amount
+
+print("Test hello!")
+ic
+
+print(xlsx)
